6490c13d25bee7ff9f1b7f037da0ef31116a7bfb
andgineer <filbert@yandex.ru>
2020-08-19 06:14:00 +0300

    DAGs is ON on creation

--- etl/default.py
+++ etl/default.py
@@ -6,7 +6,6 @@ default_args = {
     'start_date': datetime(2020, 8, 16),
     'email': ['andrey@sorokin.engineer'],
     'email_on_failure': True,
-    # 'catchup': False,  # do not auto-run for all missed dates
 }
 
 dbs_to_update = ['dev_db']
