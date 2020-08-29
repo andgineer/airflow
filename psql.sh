8579c32050bfa15c5d996d356eb0199b2d25e8b8
andgineer <filbert@yandex.ru>
2020-08-29 14:59:55 +0300

    fixed typos

--- psql.sh
+++ psql.sh
@@ -1,12 +1,23 @@
 #! /usr/bin/env bash
 #
-# run psql for ETL DB in postgres container
+# run psql for ETL/Airflow DB in postgres container
+# default is ETL DB.
+# To connect to airflow DB use
+#   ./psql.sh airflow
 #
 POSTGRES_SERVICE_NAME="airflow-db"
-CREDENTIALS="env/etl_db.env"
-. ./source_env.sh  # this line should be before we use env vars
-POSTGRES_USER=${ETL_DB_USER}
-POSTGRES_DB=${ETL_DB_DB}
+. ./export_vars.sh # this line should be before we use $ETL_DB_*
+
+case $1 in
+     airflow)
+          POSTGRES_USER=${AIRFLOW_DB_USER}
+          POSTGRES_DB=${AIRFLOW_DB_DB}
+          ;;
+     *)
+          POSTGRES_USER=${ETL_DB_USER}
+          POSTGRES_DB=${ETL_DB_DB}
+          ;;
+esac
 
 echo "Service ${POSTGRES_SERVICE_NAME} user $POSTGRES_USER data $POSTGRES_DB"
 
@@ -20,4 +31,4 @@ if container_is_not_running ${POSTGRES_SERVICE_NAME} ; then
     exit
 fi
 
-./exec.sh ${POSTGRES_SERVICE_NAME} psql -U $POSTGRES_USER -d $POSTGRES_DB "$@"
+./exec.sh ${POSTGRES_SERVICE_NAME} psql -U $POSTGRES_USER -d $POSTGRES_DB
