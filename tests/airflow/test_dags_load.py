b24afc8723e60471a54cd1cd538ac3d11328d2a6
andgineer <filbert@yandex.ru>
2020-09-08 16:54:36 +0300

    fixed load DAGs test

--- tests/airflow/test_dags_load.py
+++ tests/airflow/test_dags_load.py
@@ -5,6 +5,6 @@ def test_dags_load_with_no_errors(dag_files):
     dag_bag = DagBag(include_examples=False)
     for file_name in dag_files:
         print(f'Loading DAG {file_name}')
-        dags = dag_bag.process_file(file_name)
-        assert len(dags) > 0
+        dags = dag_bag.process_file(file_name, only_if_updated=False)
+        assert len(dags) > 0, f'Cannot load DAG {file_name}'
         assert len(dag_bag.import_errors) == 0
