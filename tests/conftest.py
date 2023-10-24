71af997371395218f22a1662a9800f08b250da66
andgineer <Andrei_Sorokin2@epam.com>
2023-10-24 08:48:29 +0200

    platform independent paths

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,10 +8,14 @@ import modules_load
 
 log = logging.getLogger()
 
-sys.path.append(os.path.join(os.path.dirname(__file__), '../etl'))
-os.environ['AIRFLOW__CORE__DAGS_FOLDER'] = './etl'
-os.environ['AIRFLOW_HOME'] = './tests/airflow_home'
-os.environ['AIRFLOW_CONFIG'] = 'tests/resources/airflow_test.cfg'
+script_dir = os.path.dirname(os.path.abspath(__file__))
+airflow_home = os.path.join(script_dir, 'airflow_home')
+etl_dir = os.path.join(script_dir, '..', 'etl')
 
-# load Airflow only after setting AIRFLOW_HOME and Dags folder
+sys.path.append(etl_dir)  # make Python 1st look for packages in ETL dir
+os.environ['AIRFLOW__CORE__DAGS_FOLDER'] = etl_dir
+os.environ['AIRFLOW_HOME'] = airflow_home
+os.environ['AIRFLOW_CONFIG'] = os.path.join(airflow_home, 'airflow_test.cfg')
+
+# load Airflow only after setting Airflow env vars
 modules_load.asterisk(Path(__file__).parent / 'fixtures', 'fixtures', globals())
--- tests/fixtures/config.py
+++ tests/fixtures/config.py
@@ -1,23 +1,20 @@
-import os
-
 import pytest
-from airflow.utils.dates import days_ago
-from airflow import models, settings
-from airflow.utils.db import initdb
-from airflow.utils import db as airflow_db
+from airflow import models
+from airflow.utils.db import resetdb
 
 
 from _config import Config
 
+
 class TestConfig(Config):
     dags_folder = 'etl'  # relative to project root where we run pytest
-
+    
+    
 @pytest.fixture(scope='session')
 def config():
-    # airflow_db.resetdb()
-    initdb()
+    resetdb()
+    yield TestConfig()
 
-    return TestConfig()
 
 @pytest.fixture(scope='session')
 def dag_bag(dag_files):
@@ -26,11 +23,10 @@ def dag_bag(dag_files):
         dag_bag.process_file(file_name, only_if_updated=False)
     return dag_bag
 
+
 @pytest.fixture
 def dag_hello_world(dag_bag):
-    result = dag_bag.get_dag('HelloPandas')
-    return result
-
+    return dag_bag.get_dag('HelloPandas')
 
 
 
