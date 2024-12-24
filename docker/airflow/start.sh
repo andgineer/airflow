39830bed7e64aa43375cdcdee005225f9bf3f5aa
andgineer <Andrei_Sorokin2@epam.com>
2024-12-24 06:31:32 +0100

    https://github.com/andgineer/airflow/issues/12 fix deprecated config

--- docker/airflow/start.sh
+++ docker/airflow/start.sh
@@ -5,7 +5,7 @@
 
 ./wait-for-postgres.sh "$AIRFLOW_DB_HOST" "$AIRFLOW_DB_PORT"
 
-airflow db init
+airflow db migrate
 airflow connections add dev_db --conn-uri "${DEV_DB_URL}"
 airflow connections add file_local --conn-extra "{\"path\": \"/ingest\"}" --conn-type fs
 airflow users  create --role Admin --username admin --email admin --firstname admin --lastname admin --password admin
