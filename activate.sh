6417ba3eaf2428cc6f5eb6634cd4370d6f1d992c
andgineer <Andrei_Sorokin2@epam.com>
2025-01-05 14:47:10 +0100

    activate conda in activate.sh

--- activate.sh
+++ activate.sh
@@ -1,7 +1,10 @@
 #! /usr/bin/env bash
+#
+# Create and/or activate Python environment for the project
+#
 
 ENV_NAME="airflow"
-PRIMARY_PYTHON_VERSION="3.11"  # sync with .github/workflows/docs.yml&static.yml
+PRIMARY_PYTHON_VERSION="3.12"  # sync with .github/workflows/docs.yml&static.yml
 ARFLOW_VERSION="2.8.2"
 
 RED='\033[1;31m'
@@ -19,30 +22,31 @@ if ! (return 0 2>/dev/null) ; then
     exit 1  # we detected we are NOT source'd so we can use exit
 fi
 
-if type conda 2>/dev/null; then
-    if conda info --envs | grep "\b${ENV_NAME}\s"; then
-      echo -e $CYAN"activating environment ${ENV_NAME}"$NC
-    else
-      if [[ -z $(conda list --name base | grep mamba) ]]; then
-        echo "..installing mamba.."
-        conda install mamba --name base -c conda-forge
-      fi
-      echo -e $CYAN"creating conda environment ${ENV_NAME}"$NC
-      conda create -y --name ${ENV_NAME} python=${PRIMARY_PYTHON_VERSION}
-      conda activate ${ENV_NAME}
-      conda install mamba -c conda-forge
-#      mamba install -y -c conda-forge rdkit
-      mamba install -y pip "dill>=0.3.6"
-      mamba install -c conda-forge airflow
-      pip install -r docker/airflow/requirements.txt
-      pip install -r test_requirements.txt
-      conda deactivate  # RE-activate conda env so python will have access to conda installed deps
-    fi
+if [[ ! -d "$HOME/anaconda3/" ]] ; then
+  echo -e $RED"(!) Please install anaconda https://docs.anaconda.com/anaconda/install/"$NC
+  return 1  # we are source'd so we cannot use exit
+fi
+
+source "$HOME/anaconda3/bin/activate"
+conda init
+
+if conda info --envs | grep "\b${ENV_NAME}\s"; then
+  echo -e $CYAN"activating environment ${ENV_NAME}"$NC
 else
-    echo
-    echo -e $RED"(!) Please install anaconda"$NC
-    echo
-    return 1  # we are source'd so we cannot use exit
+  if [[ -z $(conda list --name base | grep "^mamba ") ]]; then
+    echo -e $CYAN"..installing mamba.."$NC
+    conda install mamba --name base --channel conda-forge --yes
+  fi
+  echo -e $CYAN"..creating environment ${ENV_NAME} with ${PRIMARY_PYTHON_VERSION}.."$NC
+  conda create -y -n ${ENV_NAME} python="${PRIMARY_PYTHON_VERSION}"
+  conda activate ${ENV_NAME}
+  echo -e $CYAN"..installing dependencies.."$NC
+#      mamba install -y -c conda-forge rdkit
+  mamba install -y pip "dill>=0.3.6"
+  mamba install -c conda-forge airflow
+  pip install -r docker/airflow/requirements.txt
+  pip install -r test_requirements.txt
+  conda deactivate  # RE-activate conda env so python will have access to conda installed deps
 fi
 
 conda activate ${ENV_NAME}
